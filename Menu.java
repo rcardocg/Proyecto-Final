392d7e5bfa4fc1612bbb61e6f5555462070c4318
rcardocg <55712552+rcardocg@users.noreply.github.com>
2020-11-02 16:20:51 -0600

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,11 @@
+import APP.BD.*;
+import APP.*;
+import java.util.*;
+public class Menu{
+    public static void main(String[]args){  
+        // ig abracadabra = new ig();
+        Cliente cli = new Cliente();
+        ig r=new ig();
+        r.setVisible(true); 
+    }
+}
